e4e5940141ab67b4764c0c80a5ed5bdbdc61c41f
erpr2001 <eprachi2001@gmail.com>
2024-03-06 20:26:00 -0500

    step 6: set the delegate to a new ArrayList instead of "this"

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -3,10 +3,10 @@ package org.example;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
-public class MyStack<E> extends ArrayList<E>{
+public class MyStack<E> {
     private ArrayList<E> delegate;
     public MyStack(){
-        delegate=this;
+        delegate= new ArrayList<>();
     }
 
 
